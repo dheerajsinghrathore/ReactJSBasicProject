3fc71bdb66f4b185cac1e0b6d8e5f1034cc9dfe9
dheerajrathore <rathored070@gmail.com>
2026-01-28 09:22:09 +0530

    Pastry example code

--- React_New_Projects/ReduxToolkit/ReactRTK2/vite-project/src/Features/pastry/Pastry.jsx
+++ React_New_Projects/ReduxToolkit/ReactRTK2/vite-project/src/Features/pastry/Pastry.jsx
@@ -4,7 +4,7 @@ import { orderPastry, restockPastry } from "./pastrySlice";
 
 export default function Pastry() {
   const [pastryAvailable, setPastryAvailable] = useState(true);
-  const [restockPastries, setRestockPastries] = useState("");
+  const [restockValue, setRestockValue] = useState("");
   const pastryCount = useSelector((state) => {
     return state.pastry.numOfPastries;
   });
@@ -12,18 +12,18 @@ export default function Pastry() {
 
   const handlePastry = () => {
     if (pastryCount > 0) {
-      dispatch(orderPastry());
+      dispatch(orderPastry()); //  orderPastry -  is Action creator
     } else {
       setPastryAvailable(false);
     }
   };
 
   const handleRestockPastry = () => {
-    const restockNumber = parseInt(restockPastries, 10);
+    const restockNumber = parseInt(restockValue, 10);
     if (!isNaN(restockNumber) && restockNumber > 0) {
       dispatch(restockPastry(restockNumber));
-      setPastryAvailable(true);
-      setRestockPastries(""); // Clear input after restocking
+      setPastryAvailable(true);   // Remove error message after pastry available
+      setRestockValue(""); // Clear input after restocking
     }
   };
 
@@ -38,12 +38,12 @@ export default function Pastry() {
       <input
         type="number"
         placeholder="Enter restock amount"
-        value={restockPastries}
-        onChange={(e) => setRestockPastries(e.target.value)}
+        value={restockValue}
+        onChange={(e) => setRestockValue(e.target.value)}
         min="1"
       />
       <br />
-      <button onClick={handlePastry}>Order Pastry</button>
+      <button onClick={handlePastry}>Order 1 Pastry</button>
       <button onClick={handleRestockPastry}>Restock Pastry</button>
     </div>
   );
